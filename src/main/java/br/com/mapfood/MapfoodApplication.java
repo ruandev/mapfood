329013c5c70825135605095c92e95a4f8c2b8a03
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2019-01-21 19:42:48 -0200

    Config projeto inicial

---
+++ src/main/java/br/com/mapfood/MapfoodApplication.java
@@ -0,0 +1,14 @@
+package br.com.mapfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapfoodApplication.class, args);
+	}
+
+}
+
