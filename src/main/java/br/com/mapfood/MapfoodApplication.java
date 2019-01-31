27fc4153b46a87befb1bf5e901c1a99f5493d9e5
ruan victor silva <rvsilva@thoughtworks.com>
2019-01-31 13:12:20 -0200

    Add processamento de CSV de Produtos

--- src/main/java/br/com/mapfood/MapfoodApplication.java
+++ src/main/java/br/com/mapfood/MapfoodApplication.java
@@ -4,8 +4,6 @@ import br.com.mapfood.Service.ClienteService;
 import br.com.mapfood.Service.EstabelecimentoService;
 import br.com.mapfood.Service.MotoboyService;
 import br.com.mapfood.Service.ProdutosEstabelecimentoService;
-import br.com.mapfood.domain.ProdutosEstabelecimento;
-import br.com.mapfood.repository.ProdutosEstabelecimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +22,7 @@ public class MapfoodApplication implements CommandLineRunner {
 	private MotoboyService motoboyService;
 
 	@Autowired
-	private ProdutosEstabelecimentoRepository produtosEstabelecimentoRepository;
+	private ProdutosEstabelecimentoService produtosEstabelecimentoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapfoodApplication.class, args);
@@ -40,13 +38,6 @@ public class MapfoodApplication implements CommandLineRunner {
 		motoboyService.processarFileMotoboy();
 
 		produtosEstabelecimentoService.processarFileProdutosEstabelecimento();
-
-		ProdutosEstabelecimento p1 =  new ProdutosEstabelecimento(1L,"Galinhada executivo","00002d8401f2d47cef5bb54b4e1cea226ec5155f2b5fb5fab4163b71d3eeb281","3848076bc47f22e56d585493e80f8563872b5f6912e7647d346c51096bfb56be","Mineiro Delivery - Boa Viagem","Almoço por até R$ 13,90",12.1d,"RECIFE");
-
-		ProdutosEstabelecimento p2 =  new ProdutosEstabelecimento(2L,"Galinhada executivo",
-				"00002d8401f2d47cef5bb54b4e1cea226ec5155f2b5fb5fab4163b71d3eeb281","3848076bc47f22e56d585493e80f8563872b5f6912e7647d346c51096bfb56be","Mineiro Delivery - Boa Viagem","Almoço por até R$ 13,90",12.1d,"RECIFE");
-
-		produtosEstabelecimentoRepository.saveAll(Arrays.asList(p1,p2));
 	}
 }
 
--- src/main/java/br/com/mapfood/Service/ProdutosEstabelecimentoService.java
+++ src/main/java/br/com/mapfood/Service/ProdutosEstabelecimentoService.java
@@ -3,7 +3,9 @@ package br.com.mapfood.Service;
 import java.util.List;
 import java.util.Optional;
 
+import br.com.mapfood.domain.Motoboy;
 import br.com.mapfood.domain.ProdutosEstabelecimento;
+import br.com.mapfood.processors.ProdutosEstabelecimentoProcessor;
 import br.com.mapfood.repository.ProdutosEstabelecimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,15 @@ public class ProdutosEstabelecimentoService {
 
     @Autowired
     private ProdutosEstabelecimentoRepository produtosEstabelecimentoRepository;
+
+    @Autowired
+    private ProdutosEstabelecimentoProcessor produtosEstabelecimentoProcessor;
+
+    public void processarFileProdutosEstabelecimento(){
+        List<ProdutosEstabelecimento> listProdutosEstabelecimentos = produtosEstabelecimentoProcessor.lerArquivo();
+
+        produtosEstabelecimentoRepository.saveAll(listProdutosEstabelecimentos);
+    }
 
     public List<ProdutosEstabelecimento> findAll(){
         return produtosEstabelecimentoRepository.findAll();
